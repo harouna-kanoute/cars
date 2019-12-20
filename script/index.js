fca4f25c9a9f7ac5cbb5b5a484f2abd65659ea9f
harouna-kanoute <harounakanoute@hotmail.com>
2019-12-20 16:39:51 +0100

    modification sur le css et ajoute de pied  de page

--- script/index.js
+++ script/index.js
@@ -4,13 +4,17 @@ $(document).ready(function() {
     arrow = $('.map img')
     blockslide = $('#blockPlanSite').hide()
 
-    plansite.on('click', function(e) {
+    plansite.on('click', function() {
         blockslide.slideToggle(1000);
 
         arrow.css({
             "-webkit-transform": "rotate(180deg)",
             "-moz-transform": "rotate(180deg)",
             "transform": "rotate(180deg)" 
-        });e.preventDefault()
+        });
     });
 });
+
+
+
+
